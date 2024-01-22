d86c172bb73eadc919929aa5015c7abe653b9f64
han03 <han031121@gmail.com>
2024-01-22 18:08:13 +0900

    button-20240122

--- teinou/commands/button.py
+++ teinou/commands/button.py
@@ -1,13 +1,16 @@
 from teinou.client import deletable_command
+from teinou import ImageParser
 import discord
+
+imageParser = ImageParser('nazuna', 50)
 
 class Buttons(discord.ui.View):
     def __init__(self, *, timeout=180):
         super().__init__(timeout=timeout)
-    @discord.ui.button(label="Button",style=discord.ButtonStyle.green)
-    async def regenButton(self,buttons:discord.ui.Button,interaction:discord.Interaction):
-        await interaction.response.edit_message(content=f"This is an edited button response!")
+    @discord.ui.button(label="Nazuna",style=discord.ButtonStyle.green)
+    async def regenButton(self,interaction:discord.Interaction,buttons:discord.ui.Button):
+        await interaction.response.send_message(file = imageParser.getRandomItem())
 
 @deletable_command(name = "버튼")
 async def button(ctx):
-    return await ctx.send("This message has buttons!",view=Buttons())
+    return await ctx.send("나즈나버튼",view=Buttons())
